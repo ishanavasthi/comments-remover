8fdd52b5a9dc1fdd125cfa13418703d79cfea8b3
Ishan Avasthi <87573177+ishanavasthi@users.noreply.github.com>
2023-09-24 20:42:30 +0530

    First and Final Commit.
    This is my project for SST Hackathon 2.0

---
+++ README.md
@@ -0,0 +1,41 @@
+# Code Comment Remover
+
+Code Comment Remover is a web application that allows users to remove comments from code written in various programming languages. It provides support for languages such as C++, Java, Python, GoLang, JavaScript, Ruby, Swift, CSS, and HTML.
+
+## Features
+
+- Language Selection: Choose the programming language of your code from a dropdown menu.
+- Input Box: Enter your code into the input box.
+- Remove Comments: Click the "Remove Comments" button to eliminate comments from your code.
+- Output Display: View the code with comments removed in the output box.
+- Copy to Clipboard: Easily copy the cleaned code to your clipboard.
+- Dark Mode: Toggle between light and dark modes for a comfortable viewing experience.
+
+## Usage
+
+1. Select the desired programming language from the dropdown menu.
+2. Enter your code into the input box.
+3. Click the "Remove Comments" button to process the code.
+4. The cleaned code will appear in the output box.
+5. Use the "Copy to Clipboard" button to copy the result to your clipboard.
+
+## Installation
+
+There's no need for installation since this is a web-based application. Simply access it in your web browser.
+
+## Development
+
+This project is built using HTML, CSS, and JavaScript. You can contribute to its development by following these steps:
+
+1. Clone this repository to your local machine.
+2. Make changes and improvements.
+3. Test your changes thoroughly.
+4. Submit a pull request for review.
+
+## Author
+
+- [Ishan Avasthi](https://ishanavasthi.in?utm_source=GitHub)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ script.js
@@ -0,0 +1,105 @@
+// Get elements from the DOM
+const languageSelect = document.getElementById("languageSelect");
+const codeInput = document.getElementById("codeInput");
+const removeCommentsBtn = document.getElementById("removeCommentsBtn");
+const copyToClipboardBtn = document.getElementById("copyToClipboardBtn");
+const codeOutput = document.getElementById("codeOutput");
+const darkModeToggle = document.getElementById("darkModeToggle");
+const body = document.body;
+
+// Event listener for the "Remove Comments" button
+removeCommentsBtn.addEventListener("click", () => {
+    const selectedLanguage = languageSelect.value;
+    const inputCode = codeInput.value;
+    // Implement code comment removal logic based on the selected language
+    // For example, you can use regular expressions to remove comments.
+    // Replace the following line with the actual logic.
+    const outputCode = removeComments(inputCode, selectedLanguage);
+    codeOutput.value = outputCode;
+});
+
+// Event listener for the "Copy to Clipboard" button
+copyToClipboardBtn.addEventListener("click", () => {
+    codeOutput.select();
+    codeOutput.setSelectionRange(0, 99999); // For mobile devices
+    document.execCommand("copy");
+    alert("Code copied to clipboard!");
+});
+
+// Event listener for the dark mode toggle switch
+darkModeToggle.addEventListener("change", () => {
+    if (darkModeToggle.checked) {
+        body.classList.add("dark-mode");
+        body.classList.remove("light-mode");
+    } else {
+        body.classList.remove("dark-mode");
+        body.classList.add("light-mode");
+    }
+});
+
+if (darkModeToggle.checked) {
+    toggleDarkMode();
+}
+
+// Function to remove comments 
+function removeComments(code, language) {
+    if (language === "c++" || language === "java" || language === "javascript") {
+        // Remove C++, Java, and JavaScript comments (// and /* */)
+        code = code.replace(/\/\/.*|\/\*[\s\S]*?\*\//g, "");
+    } else if (language === "python") {
+        // Remove Python comments (# and ''')
+        code = code.replace(/#.*/g, "").replace(/'''.*?'''/gs, "");
+    } else if (language === "golang") {
+        // Remove GoLang comments (// and /* */)
+        code = code.replace(/\/\/.*|\/\*[\s\S]*?\*\//g, "");
+    } else if (language === "ruby") {
+        // Remove Ruby comments (#)
+        code = code.replace(/#.*/g, "");
+    } else if (language === "swift") {
+        // Remove Swift comments (// and /* */)
+        code = code.replace(/\/\/.*|\/\*[\s\S]*?\*\//g, "");
+    } else if (language === "css") {
+        // Remove CSS comments (/* */)
+        code = code.replace(/\/\*[\s\S]*?\*\//g, "");
+    } else if (language === "html") {
+        // Remove HTML comments (<!-- -->)
+        code = code.replace(/<!--[\s\S]*?-->/g, "");
+    }
+    return code;
+}
+
+// Function to toggle dark mode
+function toggleDarkMode() {
+    if (darkModeToggle.checked) {
+        document.body.classList.add("dark-mode");
+        localStorage.setItem("darkMode", "enabled");
+    } else {
+        document.body.classList.remove("dark-mode");
+        localStorage.setItem("darkMode", "disabled");
+    }
+}
+
+// Event listener for the dark mode toggle switch
+darkModeToggle.addEventListener("change", toggleDarkMode);
+
+// Check and apply saved dark mode preference
+const savedDarkMode = localStorage.getItem("darkMode");
+if (savedDarkMode === "enabled") {
+    darkModeToggle.checked = true;
+    toggleDarkMode();
+}
+
+// Function to set and remember the selected language
+function setLanguagePreference() {
+    const selectedLanguage = languageSelect.value;
+    localStorage.setItem("languagePreference", selectedLanguage);
+}
+
+// Event listener for the language select dropdown
+languageSelect.addEventListener("change", setLanguagePreference);
+
+// Check and apply saved language preference
+const savedLanguage = localStorage.getItem("languagePreference");
+if (savedLanguage) {
+    languageSelect.value = savedLanguage;
+}
